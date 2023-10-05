2a72d2f18525c7c57019e01bcfd8babdb3fe61e2
Olababs98 <babatundesaheed389@gmail.com>
2023-10-05 19:20:48 +0000

    manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Babatunde saheed <https://github.com/Olababs98>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
